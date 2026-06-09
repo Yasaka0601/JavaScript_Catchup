5e8d27397dbbce0c783026a568eeb1dd1011c4c8
Yasaka0601 <www.gtr.0601@icloud.com>
2026-06-09 22:05:06 +0900

    chapter5

---
+++ MenuList/js/script.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector('#menu');
+
+const content = `<div><img src="images/strawberry.jpg" alt=""></div>`;
+
+//menu.textContent = content;
+
+menu.insertAdjacentHTML('beforeend', content);
