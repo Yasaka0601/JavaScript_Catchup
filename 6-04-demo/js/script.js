dc5f73afc76a87fd84ad568f9a466ec35db62df6
Yasaka0601 <www.gtr.0601@icloud.com>
2026-06-20 17:24:36 +0900

    chapter6

---
+++ 6-04-demo/js/script.js
@@ -0,0 +1,27 @@
+const heading = document.querySelector('#heading');
+
+const keyframes = {
+    // opacity: [0, 1],
+    // rotate: ['x 360deg', 0],
+    // translate: ['0 50px', 0],
+    //color: ['#f66', '#fc2', '#0c6', '#0bd' ]
+    // color: ['transparent', '#fff'],
+    // backgroundPosition: ['100% 0', '0 0'],
+    borderRadius: [
+        '20% 50% 50% 70%/50% 50% 70% 50%',
+        '50% 20% 50% 50%/40% 40% 60% 60%',
+        '50% 40% 20% 40%/40% 50% 50% 80%',
+        '50% 50% 50% 20%/40% 40% 60% 60%',
+    ],
+};
+
+const options = {
+    duration: 8000,
+    // easing: 'ease',
+    direction: 'alternate',
+    iterations: Infinity,
+    //easing: 'ease',
+    
+};
+
+heading.animate(keyframes, options);
